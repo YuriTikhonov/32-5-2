5c78c6b9862f4f7a425247010abe736937fcbdbc
Yuri Tikhonov <tikhonov.yuri@gmail.com>
2022-09-26 16:04:58 +0300

    start1

--- main.cpp
+++ main.cpp
@@ -5,38 +5,71 @@
 #include <clocale>
 #include "nlohmann_json/include/nlohmann/json.hpp"
 
-struct Movie
+struct MovieName
 {
     std::string movieTitle;
+}m1,m2;
+
+struct Movie
+{
+    //std::string movieTitle;
     std::string country;
     std::string productionDate;
     std::string company;
     std::string scriptAuthor;
     std::string director;
-    std::string producer;
+    //std::string producer;
     std::unordered_map<std::string,std::string>actors;
-};
+}movie1,movie2;//,movie3,movie4,movie5;
 
 int main()
 {
     setlocale(LC_CTYPE, "rus");
-    std::vector<Movie> movie(5);
+    //std::vector<Movie> movie(2);
+
     std::ofstream file("movie.json");
-    nlohmann::json dict = {
-            {"movie title",movie[0].movieTitle = "Место встречи изменить нельзя"},
-            {"country",movie[0].country = "СССР"},
-            {"productionDate",movie[0].productionDate = "1979"},
-            {"company",movie[0].company = "Одесская киностудия"},
-            {"scriptAuthor",movie[0].scriptAuthor = "Станислав Говорухин"},
-            {"Actors:", movie[0].actors = {{"Глеб Жеглов",    "Владимир Высоцкий"},
+    //nlohmann::json data = {MovieName.mo,Movie};
+    nlohmann::json dict = nlohmann::json::array();
+    m1 = {"Место встречи изменить нельзя"};
+    m2 = {"Белое солнце пустыни"};
+
+    movie1 = {
+            //{"movie title",movie1.movieTitle = "Место встречи изменить нельзя"},
+            {movie1.country = "СССР"},
+            {movie1.productionDate = "1979"},
+            {movie1.company = "Одесская киностудия"},
+            {movie1.scriptAuthor = "Станислав Говорухин"},
+            {movie1.director = "Станислав Говорухин"},
+            { movie1.actors = {{"Глеб Жеглов",    "Владимир Высоцкий"},
                                         {"Володя Шарапов", "Владимир Конкин"},
                                         {"Груздев",        "Сергей Юрский"},
                                         {"Пётр Ручников",  "Евгений Евстигнеев"},
                                         {"Манька Облигация","Лариса Удовиченко"}
             }
             }
+
     };
-    file << dict;
+
+     movie2 = {
+            //{"movie title",movie2.movieTitle = "Белое солнце пустыни"},
+            {movie2.country = "СССР"},
+            {movie2.productionDate = "1969"},
+            {movie2.company = "Мосфильм, Ленфильм"},
+            {movie2.scriptAuthor = "Валентин Ежов\n"
+                                                  "Рустам Ибрагимбеков\n"
+                                                  "Марк Захаров"},
+            {movie2.director = "Владимир Мотыль"},
+            {movie2.actors = {{"Фёдор Сухов",    "Анатолий Кузнецов"},
+                                           {"Саид", "Спартак Мишулин"},
+                                           {"Абдулла",        "Кахи Кавсадзе"},
+                                           {"Верещагин",  "Павел Луспекаев"},
+                                           {"Гюльчатай","Татьяна Федотова"},
+                                         {"Петруха","Николай Годовиков"}
+            }
+            }
+    };
+
+    file << dict[0] << dict[1];
 
     return 0;
 }
