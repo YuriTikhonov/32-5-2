3cf8c35b683e26fccd694957f71261e5147b7694
Yuri Tikhonov <tikhonov.yuri@gmail.com>
2022-10-04 19:51:28 +0300

    divideIt

--- main.cpp
+++ main.cpp
@@ -1,27 +1,64 @@
 #include <iostream>
 #include <string>
 #include <fstream>
-#include <vector>
+#include <unordered_map>
 #include "nlohmann_json/include/nlohmann/json.hpp"
-using nlohmann::json;
+
+struct Movie
+{
+    std::string movieTitle;
+    std::string country;
+    int productionDate = 0;
+    std::string company;
+    std::string scriptAuthor;
+    std::string director;
+    //std::string producer;
+    std::unordered_map<std::string,std::string>actors;
+}movie,movie2;//,movie3,movie4,movie5;
 
 int main()
 {
-    std::vector <json> alljSon;
+     /*std::ofstream file("movie.json");
+      nlohmann::json dict = {
+              {"Movie title",movie.movieTitle = "Mesto vstrechi izmenit nelzya"},{
+                  {"Country",movie.country = "USSR"},
+                  {"Production date",movie.productionDate = 1979},
+                  {"Company",movie.company = "Odessa kinokompany"},
+                  {"Script author",movie.scriptAuthor = "Stanislav Govorukhin"},
+                      {"Director",movie2.director = "Some Name"},
+                  {"Actors:", movie.actors = {{"Gleb Zheglov",    "Vladimir Vysotskiy"},
+                                              {"Volodya Sharapov", "Vladimir Konkin"},
+                                              {"Grudsev",        "Sergey Yurskiy"},
+                                              {"Petr Ruchnikov ",  "Evgeniy Evstigneev"},
+                                              {"Man'ka Obligatsiya","Larisa Udovichenko"}
+                      }
+                  }},
+              {"Movie title",movie2.movieTitle = "Beloye Solnze pustyni"},{
+               {"Country",movie2.country = "USSR"},
+               {"Production date",movie2.productionDate = 1969},
+                      {"Company",movie2.company = "Mosfolm, Lenfilm"},
+                      {"Script author",movie2.scriptAuthor = "Valentin Ezhov\n"
+                                             "Rustam Ibragimbekov\n"
+                                             "Mark Zakharov"},
+                      {"Director",movie2.director = "Vladimir Motyl"},
+                      {"Actors:",movie2.actors = {{"Fyodor Sukhov",    "Anatoliy Kuznetzov"},
+                                        {"Said", "Spartak Mishulin"},
+                                        {"Abdulla",        "Kakhi Kavasadze"},
+                                        {"Vereschagin",  "Pavel Luspekaev"},
+                                        {"Gyul'chatay","Tatyana Fedotova"},
+                                        {"Petrukha","Nikolay Godovikov"}
+                      }
+                      }}
+
+      };
+
+      file << dict;*/
+
     std::ifstream i("movie.json");
-    json j = json::parse(i);
-    for (auto it = j.begin(); it != j.end(); ++it)
-    {
-        if (j.is_array())
-        {
-            alljSon.push_back(it.value());
-        }
-        else
-        {
-            // If it's not an array, it's an object, and from what I understood you want the key too.
-            alljSon.push_back({it.key(), it.value()});
-        }
+    nlohmann::json j;
+    i >> j;
+
+    for (nlohmann::json::iterator it = j.begin(); it != j.end(); ++it) {
+        std::cout << *it << '\n';
     }
-    std::cout << alljSon << " ";
-    return 0;
 }
